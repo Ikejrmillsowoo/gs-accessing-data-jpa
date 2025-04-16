fd0275b569962f7ee1a5f3cd38c42a90a7ced5ba
Isaac Mills-Owoo <poppymills2@gmail.com>
2025-04-16 15:48:16 -0400

    customer repo created and updated accessing data application

--- initial/src/main/java/com/example/accessingdatajpa/CustomerRepository.java
+++ initial/src/main/java/com/example/accessingdatajpa/CustomerRepository.java
@@ -1,4 +1,12 @@
 package com.example.accessingdatajpa;
 
-public class CustomerRepository {
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+
+    List<Customer> findByLastName(String LastName);
+
+    Customer findById(long id);
 }
